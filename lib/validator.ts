f63f305c43842cac4b066ee663700ad4807ba498
Lê  Hoàng Vũ <68913@thegioididong.com>
2023-12-29 00:57:14 +0700

    form create event

---
+++ lib/validator.ts
@@ -0,0 +1,19 @@
+import * as z from 'zod';
+
+export const eventFormSchema = z.object({
+  title: z.string()
+    .min(3, "Title must be at least 3 charaters."),
+  description: z.string()
+    .min(3, "Description must be at least 3 charaters.")
+    .max(400, 'Description must be less than 400 charaters.'),
+  location: z.string()
+    .min(3, "Location must be at least 3 charaters.")
+    .max(400, 'Location must be less than 400 charaters.'),
+  imageUrl: z.string(),
+  startDateTime: z.date(),
+  endDateTime: z.date(),
+  categoryId: z.string(),
+  price: z.string(),
+  isFree: z.boolean(),
+  url: z.string().url(),
+})
